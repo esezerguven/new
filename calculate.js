d58691e47cb36254e8a9a5a35542187ea6570af1
Sezer Güven <esezerguven@gmail.com>
2023-05-06 15:31:18 +0300

    week 3

--- calculate.js
+++ calculate.js
@@ -1,3 +1,31 @@
+/*
+setTimeout(function() {
+    console.log('started')
+    document.getElementById("7").addEventListener("click", function () {
+        calculate('7');
+    });
+}, 5000); // msecond
+*/
+
+
+window.addEventListener('load', function() {   //page loaded event
+    /*
+    document.getElementById("7").addEventListener("click", function () {
+        calculate('7');
+    });
+    */
+
+    var buttons = document.getElementsByTagName('button');
+
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function(event) {
+            calculate(event.target.id);
+            console.log('button clicked')
+        });
+    }
+});
+
+
 function calculate (input) {
     var islemler = ['-', '+', '*', '/', ','];
     var screenText = document.getElementsByClassName('screen')[0].innerText;
@@ -13,6 +41,14 @@ function calculate (input) {
         if(screenText !== '0' && !islemler.includes(lastCharacterOfScreenText)) {
             document.getElementsByClassName('screen')[0].innerText += input
         }
+    } else if (input.toLowerCase().trim() === 'clear') {
+        document.getElementsByClassName('screen')[0].innerText = 0
+    } else {   
+        try {
+            document.getElementsByClassName('screen')[0].innerText = eval(screenText)
+        } catch (error) {
+            document.getElementsByClassName('screen')[0].innerText = 'Error'
+        }  
     }
 }
 
